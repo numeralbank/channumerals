f3a3d119638a5a43e0d7f07745aab777c746d1bf
chrzyki <christoph@functor-argument.net>
2018-10-04 12:58:34 +0200

    Download raw data with wget and call. Is there a cleaner, more pythonic, way for mirroring a website?

--- lexibank_numerals.py
+++ lexibank_numerals.py
@@ -3,30 +3,18 @@ from __future__ import unicode_literals, print_function
 
 from clldutils.path import Path
 from pylexibank.dataset import Dataset as BaseDataset
+from subprocess import call
 
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
+    id = 'numerals'
 
     def cmd_download(self, **kw):
-        """
-        Download files to the raw/ directory. You can use helpers methods of `self.raw`, e.g.
-
-        >>> self.raw.download(url, fname)
-        """
-        pass
+        call(["wget", "--mirror", "--convert-links", "--adjust-extension",
+              "--page-requisites", "-P", "raw", '-nd', '-q',
+              "https://mpi-lingweb.shh.mpg.de/numeral/"])
 
     def cmd_install(self, **kw):
-        """
-        Convert the raw data to a CLDF dataset.
-
-        Use the methods of `pylexibank.cldf.Dataset` after instantiating one as context:
-
-        >>> with self.cldf as ds:
-        ...     ds.add_sources(...)
-        ...     ds.add_language(...)
-        ...     ds.add_concept(...)
-        ...     ds.add_lexemes(...)
-        """
-        with self.cldf as ds:
+        with self.cldf as _:
             pass

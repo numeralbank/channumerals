3fea066a86d767d01b3e07f3a763f160541066ac
chrzyki <christoph@functor-argument.net>
2018-10-05 14:50:51 +0200

    More table processing.

--- process_html.py
+++ process_html.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from clldutils.path import walk
+from number_parser import parse_number
 
 TABLE_IDENTIFIER = 'MsoTableGrid'
 SKIP = ['How-to-view-EN.htm', 'How-to-view-CH.htm']
@@ -12,6 +13,32 @@ def iterate_files():
 
 def find_tables(files):
     for f in files:
-        parsed = BeautifulSoup(f.read_text())
-        yield (f.name, parsed.find_all('table', {'class': TABLE_IDENTIFIER}))
+        parsed = BeautifulSoup(f.read_text(), 'html.parser')
 
+        # Returns (Language name, ResultSet) pairs. ResultSet is the set of
+        # tables that have TABLE_IDENTIFIER.
+        yield (f.stem, parsed.find_all('table', {'class': TABLE_IDENTIFIER}))
+
+
+def parse_number_table(table):
+    table_elements = table.find_all('tr')
+    elements = []
+
+    for row in table_elements:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+
+        for ele in cols:
+            if ele:
+                elements.append(ele)
+
+    return elements
+
+
+def find_number_table(tables):
+        for table in tables:
+            for element in table:
+                try:
+                    parse_number(element)
+                except ValueError:
+                    continue

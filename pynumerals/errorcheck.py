fae347cbb1d36cbb5115756611239bd5d39faccc
SimonGreenhill <simon@simon.net.nz>
2019-09-19 12:04:54 +0200

    adding more checks

--- pynumerals/errorcheck.py
+++ pynumerals/errorcheck.py
@@ -9,11 +9,30 @@ def _check_loanword(value):
     return '>' in value
 
 
+def _check_is_language(value):
+    """Checks if the value points to another language"""
+    return value.lower in ['english', 'french', 'german', 'spanish', 'portuguese']
+
+
+def _check_is_numeric(value):
+    """Checks if the value is only numerical"""
+    try:
+        int(value)
+        return True
+    except:
+        return False
+
+
+def _check_or(value):
+    return ' or ' in value
+
 
 _checkers = [
      _check_fullstop,
      _check_loanword,
-     
+     _check_is_numeric,
+     _check_is_language,
+     _check_or,
 ]
 
 
